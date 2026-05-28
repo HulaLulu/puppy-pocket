dd7cbbb1f730e5da4f3f63495284769e7b3b4312
HulaLula <anastasiaturner2009@icloud.com>
2026-05-28 14:53:49 -0400

    fixed stuff

--- src/main.js
+++ src/main.js
@@ -4,8 +4,43 @@ let clicker = document.getElementById("clicker")
 
 let scoreDisplay = document.getElementById("scoreDisplay")
 
+function updateScore (amount) {
+  score += amount
+  scoreDisplay.innerText = score + '   PawPrints'
+}
+
 clicker.addEventListener("click", function() {
-  score++
-  
-  scoreDisplay.innerText = score
-} )
+  let clickAmt = 1 + (evas ** 3.05)
+
+  updateScore(clickAmt)
+})
+
+let upgradeOne = document.getElementById("upgradeOne")
+let upgradeOneCount = document.getElementById("upgradeOneCount")
+let evas = 0
+
+upgradeOne.addEventListener("click", function() {
+  if (score >= 100) {
+    updateScore(-100)
+    evas++
+    upgradeOneCount.innerText = evas + '   evas'
+  } else {
+    alert('Not Enough PawPrints!')
+  }
+
+let upgradeTwo = document.getElementById("upgradeTwo")
+let upgradeTwoCount = document.getElementById("upgradeTwoCount")
+let georges = 0
+})
+
+upgradeTwo.addEventListener("click", function() {
+  if (score >= 4000) {
+    updateScore(-4000)
+    georges++
+    upgradeTwoCount.innerText = georges + '   georges'
+  } else {
+    alert('Not Enough PawPrints!')
+  }
+
+})
+
